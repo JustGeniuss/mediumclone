a839c179c3487b9131b6665257a4b6949208ace0
JustGeniuss <danila.kukushkin98@gmail.com>
2022-11-17 13:08:33 +0300

    change work with orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,36 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TagModule } from './tag/tag.module';
-import {TypeOrmModule} from '@nestjs/typeorm';
-import ormconfig from './ormconfig';
+import { TypeOrmModule, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import ormconfig from '../ormconfig';
 import { UserModule } from './user/user.module';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserEntity } from './user/user.entity';
+import { TagEntity } from './tag/tag.entity';
 @Module({
-  imports: [TypeOrmModule.forRoot(ormconfig), TagModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TagModule,
+    UserModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: configService.get('DB_TYPE'),
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          entities: [UserEntity, TagEntity],
+          synchronize: configService.get('DB_SYNC') === 'false',
+          keepConnectionAlive: true,
+        } as TypeOrmModuleAsyncOptions;
+      },
+    }),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    })
+  ]
 })
 export class AppModule {}
