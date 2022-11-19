f951704753fdf727f5b797b6feffda81937f89a6
JustGeniuss <danila.kukushkin98@gmail.com>
2022-11-19 18:56:42 +0300

    add custom User Decorator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,30 +1,43 @@
-import {Controller, Post, Body, UsePipes, ValidationPipe, Get, Req} from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UsePipes,
+  ValidationPipe,
+  Get,
+  Req,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.dto';
 import { LoginUserDto } from './dto/loginUser.dto';
 import { UserResponseInterface } from './types/userResponse.interface';
 import { UserService } from './user.service';
-import {Request} from 'express';
+import { Request } from 'express';
 import { ExpressRequestInterface } from 'src/types/expressRequest.interface';
+import { User } from './decoratots/user.decorator';
+import { UserEntity } from './user.entity';
 @Controller()
 export class UserController {
-    constructor (private readonly userService: UserService) {}
-    @Post('users')
-    @UsePipes(new ValidationPipe())
-    async createUser(@Body('user') createUserDto: CreateUserDto): Promise<UserResponseInterface> {
-        const user = await this.userService.createUser(createUserDto);
-        return this.userService.buildUserResponse(user);
-    }
-    
-    
-    @Post('users/login')
-    @UsePipes(new ValidationPipe())
-    async login(@Body('user') loginUserDto: LoginUserDto): Promise<UserResponseInterface> {
-        const user = await this.userService.login(loginUserDto)
-        return this.userService.buildUserResponse(user);
-    }
+  constructor(private readonly userService: UserService) {}
+  @Post('users')
+  @UsePipes(new ValidationPipe())
+  async createUser(
+    @Body('user') createUserDto: CreateUserDto,
+  ): Promise<UserResponseInterface> {
+    const user = await this.userService.createUser(createUserDto);
+    return this.userService.buildUserResponse(user);
+  }
 
-    @Get('user')
-    async currentUser(@Req() request: ExpressRequestInterface): Promise<UserResponseInterface> {
-        return this.userService.buildUserResponse(request.user);
-    }
+  @Post('users/login')
+  @UsePipes(new ValidationPipe())
+  async login(
+    @Body('user') loginUserDto: LoginUserDto,
+  ): Promise<UserResponseInterface> {
+    const user = await this.userService.login(loginUserDto);
+    return this.userService.buildUserResponse(user);
+  }
+
+  @Get('user')
+  async currentUser(@User() user: UserEntity): Promise<UserResponseInterface> {
+    return this.userService.buildUserResponse(user);
+  }
 }
